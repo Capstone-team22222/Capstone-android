406d3d11aa287366a549fc831cec1d8fd07668a7
Langdol <kms0208228@naver.com>
2024-04-15 13:01:47 +0900

    first commit

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,42 +1,60 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:alarm/alarm.dart';
+
 import 'package:flutter_application_1/screens/main_screen.dart';
-import 'screens/alarm_screen.dart';
+import 'package:flutter_application_1/screens/alarm_screen.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
+  await Alarm.init(showDebugLogs: true);
+
+  runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget{
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
-  State<MyApp> createState() => _MainScreenState();
+  _MyAppState createState() => _MyAppState();
 }
 
-class _MainScreenState extends State<MyApp>{
+class _MyAppState extends State<MyApp> {
   int currentIndex = 0;
   final screens = [
     MainPage(),
     AlarmPage(),
   ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(useMaterial3: false),
       debugShowCheckedModeBanner: false,
-      title: '수면 추적',
-      home: Scaffold(backgroundColor: Color(0xffe9edf3),
-      appBar: AppBar(
-        elevation: 0,
-        centerTitle: true,
-        backgroundColor: Colors.grey,
-        title: Text("수면 추적",
-         style: TextStyle(
-          color: Colors.black,),
+      home: Scaffold(
+        backgroundColor: Color(0xffe9edf3),
+        appBar: AppBar(
+          elevation: 0,
+          centerTitle: true,
+          backgroundColor: Colors.grey,
+          title: Text(
+            "수면 추적",
+            style: TextStyle(
+              color: Colors.black,
+            ),
           ),
         ),
         body: screens[currentIndex],
-        bottomNavigationBar:
-          BottomNavigationBar(backgroundColor: Colors.grey, selectedItemColor: Colors.white,unselectedItemColor: Colors.black,
-          currentIndex: currentIndex, onTap: (index) => setState(() => currentIndex = index), items:[
+        bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Colors.grey,
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.black,
+          currentIndex: currentIndex,
+          onTap: (index) => setState(() => currentIndex = index),
+          items: [
             BottomNavigationBarItem(
               icon: Icon(Icons.home),
               label: "홈",
